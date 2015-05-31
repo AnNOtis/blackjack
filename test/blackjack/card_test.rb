b78e162ec52a4c0a4c8323a1edcfdcab3a155dea
Otis <altispec@gmail.com>
2015-05-31 17:07:11 +0800

    add card test

---
+++ test/blackjack/card_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+require 'blackjack'
+
+class TestCard < MiniTest::Test
+
+  def setup
+    @unflipped_card = Blackjack::Card.new('J')
+    @card_ace = Blackjack::Card.new('A')
+  end
+
+  def test_all_names
+    expect_names = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+
+    assert_equal expect_names, Blackjack::Card.all_names
+  end
+
+  def test_default_card_is_unflipped
+    refute @unflipped_card.flipped
+    refute @unflipped_card.flipped?
+  end
+
+  def test_flip
+    assert @unflipped_card.flip.flipped?
+  end
+
+  def test_flip_should_return_self
+    assert_same @unflipped_card.flip, @unflipped_card
+  end
+
+  def test_point
+    assert_equal 1, Blackjack::Card.new('A').point
+    assert_equal 10, Blackjack::Card.new('J').point
+  end
+
+  def test_equal_sign
+    assert @card_ace == Blackjack::Card.new('A')
+    refute @card_ace == Blackjack::Card.new('J')
+  end
+
+  def test_to_s
+    assert_equal 'A', @card_ace.to_s
+  end
+
+end
